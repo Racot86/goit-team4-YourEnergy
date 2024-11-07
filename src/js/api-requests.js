2b4c1d65a72a0c94208e507a7f3891ad7c73e166
mariashubna <mariashubna@gmail.com>
2024-11-07 21:03:57 +0100

    .

--- src/js/api-requests.js
+++ src/js/api-requests.js
@@ -1,7 +1,14 @@
 import { filtersUrl } from './api-service';
 import axios from 'axios';
 
-export async function getCategories() {
-    const { data } = await axios.get(`${filtersUrl()}`);
-    return data; 
-  }
+export async function getCategories(page = 1, limit = 12) {
+  const isMobile = window.innerWidth <= 768; 
+  limit = isMobile ? 9 : limit;
+  const { data } = await axios.get(filtersUrl(), {
+    params: {
+      page: page,
+      limit: limit
+    },
+  });
+  return data;
+}

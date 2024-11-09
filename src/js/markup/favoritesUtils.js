e62e2bc606418eead23a9a164885e5e49a81bac6
Andrii <88sushilnikov@gmail.com>
2024-11-10 01:41:20 +0200

    favorites in localStorage

--- src/js/markup/favoritesUtils.js
+++ src/js/markup/favoritesUtils.js
@@ -1,7 +1,5 @@
 export function removeFromFavorites(exerciseId) {
   let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  console.log('Current favorites before removal:', favorites); // Логування перед видаленням
   favorites = favorites.filter(item => item.id !== exerciseId);
-  console.log('Favorites after removal:', favorites); // Логування після видалення
   localStorage.setItem('favorites', JSON.stringify(favorites));
 }
